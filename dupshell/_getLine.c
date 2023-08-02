2c7005959c68f609cf81375025c465261cb8f622
awaized1 <aduolaayomide@gmail.com>
2023-08-01 20:38:18 -0700

    updated

--- dupshell/_getLine.c
+++ dupshell/_getLine.c
@@ -1,19 +1,20 @@
 #include "shell.h"
 
+ssize_t _getline(char **lineptr, size_t *n, FILE *stream);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 void assign_lineptr(char **lineptr, size_t *n, char *buffer, size_t b);
-ssize_t _getline(char **lineptr, size_t *n, FILE *stream);
 
 /**
- * _realloc - Reallocates a memory block using malloc and free.
- * @ptr: A pointer to the memory previously allocated.
- * @old_size: The size in bytes of the allocated space for ptr.
- * @new_size: The size in bytes for the new memory block.
+ * _realloc - Code rallocates memory block using malloc and free.
+ * @old_size: Size in bytes of allocated space to pointer.
+ * @ptr: Pointer to memory previously allocated.
+ * @new_size: Size for new memory block.
  *
  * Return: If new_size == old_size - ptr.
  *         If new_size == 0 and ptr is not NULL - NULL.
- *         Otherwise - a pointer to the reallocated memory block.
+ *         Otherwise - Pointer to reallocated mem block.
  */
+
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	void *mem;
@@ -56,12 +57,13 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 }
 
 /**
- * assign_lineptr - Reassigns the lineptr variable for _getline.
- * @lineptr: A buffer to store an input string.
- * @n: The size of lineptr.
- * @buffer: The string to assign to lineptr.
- * @b: The size of buffer.
+ * assign_lineptr - Code reassigns linepointer variable for _getline.
+ * @b: Size of buffer.
+ * @lineptr: Buffer to store a string.
+ * @n: Size of linepointer.
+ * @buffer: String to assign to linepointer.
  */
+
 void assign_lineptr(char **lineptr, size_t *n, char *buffer, size_t b)
 {
 	if (*lineptr == NULL)
@@ -88,19 +90,20 @@ void assign_lineptr(char **lineptr, size_t *n, char *buffer, size_t b)
 }
 
 /**
- * _getline - Reads input from a stream.
- * @lineptr: A buffer to store the input.
- * @n: The size of lineptr.
- * @stream: The stream to read from.
+ * _getline - Code reads input from stream.
+ * @lineptr: Buffer to save the input.
+ * @n: Size of linepointer.
+ * @stream: Stream to be read.
  *
- * Return: The number of bytes read.
+ * Return: Number of bytes read.
  */
+
 ssize_t _getline(char **lineptr, size_t *n, FILE *stream)
 {
 	static ssize_t input;
 	ssize_t ret;
 	char c = 'x', *buffer;
-	int r;
+	int a;
 
 	if (input == 0)
 		fflush(stream);
@@ -114,13 +117,13 @@ ssize_t _getline(char **lineptr, size_t *n, FILE *stream)
 
 	while (c != '\n')
 	{
-		r = read(STDIN_FILENO, &c, 1);
-		if (r == -1 || (r == 0 && input == 0))
+		a = read(STDIN_FILENO, &c, 1);
+		if (a == -1 || (a == 0 && input == 0))
 		{
 			free(buffer);
 			return (-1);
 		}
-		if (r == 0 && input != 0)
+		if (a == 0 && input != 0)
 		{
 			input++;
 			break;
@@ -136,7 +139,7 @@ ssize_t _getline(char **lineptr, size_t *n, FILE *stream)
 	assign_lineptr(lineptr, n, buffer, input);
 
 	ret = input;
-	if (r != 0)
+	if (a != 0)
 		input = 0;
 	return (ret);
 }
